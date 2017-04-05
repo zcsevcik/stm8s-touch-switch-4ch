23e4588a6a04877bb700365bcd687077c90b55d2
Radek Ševčík <zcsevcik@gmail.com>
2017-04-05 18:51:55 +0200

    Add io_writepin() instead of preprocessor magic

--- io.c
+++ io.c
@@ -17,19 +17,25 @@ io_read(void)
 	return (uint8_t)GPIO_ReadInputPin(GPIOC, GPIO_PIN_7|GPIO_PIN_6|GPIO_PIN_5|GPIO_PIN_4) >> 4;
 }
 
+static void
+io_writepin(GPIO_TypeDef* GPIOx, GPIO_Pin_TypeDef GPIO_Pin, BitStatus PinState)
+{
+	if (PinState == RESET)
+	{
+		GPIO_WriteLow(GPIOx, GPIO_Pin);
+	}
+	else
+	{
+		GPIO_WriteHigh(GPIOx, GPIO_Pin);
+	}
+}
+
 void
 io_write(uint8_t val)
 {
 	// active low
-	
-#define io_bitset(BitStatus, Port, Pin) do { \
-	void (*pfnGpioWrite)(GPIO_TypeDef*, GPIO_Pin_TypeDef) = &GPIO_WriteHigh; \
-	if (!!(BitStatus)) pfnGpioWrite = &GPIO_WriteLow; \
-	pfnGpioWrite((Port), (Pin)); \
-} while (0)
-
-	io_bitset(val & (1 << 0), GPIOD, GPIO_PIN_3);
-	io_bitset(val & (1 << 1), GPIOD, GPIO_PIN_2);
-	io_bitset(val & (1 << 2), GPIOC, GPIO_PIN_3);
-	io_bitset(val & (1 << 3), GPIOA, GPIO_PIN_3);
+	io_writepin(GPIOD, GPIO_PIN_3, (BitStatus)!(val & (1<<0)));
+	io_writepin(GPIOD, GPIO_PIN_2, (BitStatus)!(val & (1<<1)));
+	io_writepin(GPIOC, GPIO_PIN_3, (BitStatus)!(val & (1<<2)));
+	io_writepin(GPIOA, GPIO_PIN_3, (BitStatus)!(val & (1<<3)));
 }
